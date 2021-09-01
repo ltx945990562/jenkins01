dad9f0156ed5dba8ba5f5d4bc7a63e1f5d3d16dd
snail <945990562@qq.com>
2021-09-01 16:58:05 +0800

    x

---
+++ src/test/java/testcases/LogIn.java
@@ -0,0 +1,10 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class LogIn {
+    @Test
+    public void logInSuccess(){
+        System.out.println("登陆成功");
+    }
+}
---
+++ src/test/java/testcases/register.java
@@ -0,0 +1,10 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class register {
+    @Test
+    public void registerSuccess(){
+        System.out.println("注册成功");
+    }
+}
