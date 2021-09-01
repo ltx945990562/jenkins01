3732bbae13ef35e098294fb832a04bafe9896967
snail <945990562@qq.com>
2021-09-01 17:05:11 +0800

    ttttt

--- src/test/java/testcases/register.java
+++ src/test/java/testcases/register.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class register {
     @Test
     public void registerSuccess(){
-        System.out.println("注册成功了");
+        System.out.println("注册成功了aaa");
     }
 }
